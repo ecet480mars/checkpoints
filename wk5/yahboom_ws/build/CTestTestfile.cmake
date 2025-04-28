943670a3d85563b16e8eab9cdf9abd8006608cd0
asdfwyay <0asdfwyay0@gmail.com>
2025-04-28 14:54:36 -0400

    setup package structure

---
+++ wk5/yahboom_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/na867/checkpoints/wk5/yahboom_ws/src
+# Build directory: /home/na867/checkpoints/wk5/yahboom_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
